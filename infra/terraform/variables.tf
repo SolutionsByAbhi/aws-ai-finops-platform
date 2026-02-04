2eff962abdd19d8bc9ad848a85ba99690393b77b
CloudNexus <abhighnagiri@gmail.com>
2026-02-04 13:21:30 +0530

    Create variables.tf

---
+++ infra/terraform/variables.tf
@@ -0,0 +1,9 @@
+variable  "region"  {
+   type              =  string
+   description  =  "AWS region"
+}
+
+variable "prefix"  {
+   type              =  string
+   description  =  "Name  prefix for  resources"
+}
